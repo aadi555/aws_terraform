5741591f7bc4fd7d6cf0019bdb1c94939de41b68
aadi555 <38933135+aadi555@users.noreply.github.com>
2021-03-05 17:29:44 +0530

    Update main.tf

--- Terraform/Modules/crawford/main.tf
+++ Terraform/Modules/crawford/main.tf
@@ -1,4 +1,10 @@
 provider "aws" {
   region     = var.region
 }
-
+terraform {
+  backend "s3" {
+    bucket = "testpipelineec2"
+    key    = "tf/terraform.tfstate"
+    region = var.region
+  }
+}
